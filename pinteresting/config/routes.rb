5268e6a89c3b5268369344b0006300ae618ff7af
Tesa <Tmaranzano@gmail.com>
2014-03-30 12:59:59 -0400

    added capistrano to gemfile

--- pinteresting/config/routes.rb
+++ pinteresting/config/routes.rb
@@ -2,6 +2,7 @@ Pinteresting::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
   get "faq" => "pages#faq"
+  get "Login" => "pages#login"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
