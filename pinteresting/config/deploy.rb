0716b27f34beb9bf95be07daba1fc895dadda40b
Tesa <Tmaranzano@gmail.com>
2014-03-30 13:56:39 -0400

    changed deploy file to try to get cap running

--- pinteresting/config/deploy.rb
+++ pinteresting/config/deploy.rb
@@ -1,18 +1,33 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'Pinteresting'
+set :repo_url, 'git@github.com:teresanzano/onemonthrails.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/root/onemonthrails/pinteresting'
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
+set :branch, "master"
 
+
+set :rails_env, "Dev"
+
+
+set :deploy_via, :remote_cache
+
+set :ssh_options, { :forward_agent => true, :port => 4321
+
+set :keep_releases, 5
+
+default_run_options[:pty] = true
+
+#http://robmclarty.com/blog/how-to-deploy-a-rails-4-app-with-git-and-capistrano
+server "128.199.193.20", :app, :web, :db, :primary => true
 # Default value for :format is :pretty
 # set :format, :pretty
 
