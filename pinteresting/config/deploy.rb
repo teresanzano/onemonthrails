de9f1fd32faf3497a5fb66470085162ddfe75c27
Tesa <Tmaranzano@gmail.com>
2014-03-30 21:32:20 -0400

    Revert "more capistrano attempts"
    This reverts commit 760981a0ec3e0d19d95a7c81ce16f532e056179a.

--- pinteresting/config/deploy.rb
+++ pinteresting/config/deploy.rb
@@ -1,35 +1,47 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+# config valid only for Capistrano 3.1
+lock '3.1.0'
 
-role :web, "162.243.236.170"                 
-role :app, "162.243.236.170"                        
-role :db,  "162.243.236.170", :primary => true 
+set :application, 'Pinteresting'
+set :repo_url, 'git@github.com:teresanzano/onemonthrails.git'
 
-set :user, "rails"
-set :password, "X54GFxv5h2"
-
-set :use_sudo, false
-set :deploy_to, "/home/rails/"
-set :deploy_via, :copy
-
-set :normalize_asset_timestamps, false
-
-require 'bundler/capistrano'
-require "rvm/capistrano"
-set :rvm_type, :system
+# Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-# set :deploy_to, '/var/www/my_app'
-# set :scm, :git
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/root/onemonthrails/pinteresting'
+set :use_sudo, false
 
+# Default value for :scm is :git
+set :scm, :git
+set :branch, "master"
+set :rails_env, "Production"
+set :deploy_via, :remote_cache
+set :ssh_options, { :forward_agent => true, :port => 4321
+set :keep_releases, 5
+
+default_run_options[:pty] = true
+
+#http://robmclarty.com/blog/how-to-deploy-a-rails-4-app-with-git-and-capistrano
+server "128.199.193.20", :app, :web, :db, :primary => true
+# Default value for :format is :pretty
 # set :format, :pretty
+
+# Default value for :log_level is :debug
 # set :log_level, :debug
+
+# Default value for :pty is false
 # set :pty, true
 
+# Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
+# Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
 # set :keep_releases, 5
 
 namespace :deploy do
@@ -42,6 +54,8 @@ namespace :deploy do
     end
   end
 
+  after :publishing, :restart
+
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
@@ -50,7 +64,5 @@ namespace :deploy do
       # end
     end
   end
-
-  after :finishing, 'deploy:cleanup'
 
 end
