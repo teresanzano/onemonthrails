73c3de7b87010926b0a16a836a2d365dfcc8466b
Tesa <Tmaranzano@gmail.com>
2014-04-02 17:11:21 -0400

    changes to deploy.rb

--- pinteresting/config/deploy.rb
+++ pinteresting/config/deploy.rb
@@ -31,7 +31,7 @@ lock '3.1.0'
 # set :deploy_to, '/var/www/my_app'
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
 
 # Default value for :format is :pretty
 # set :format, :pretty
