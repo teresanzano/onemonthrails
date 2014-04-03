6ad83cc5ddd96378342bac1966571ff33fe45f53
Tesa <Tmaranzano@gmail.com>
2014-04-03 18:43:33 -0400

    Devise changes.
    Missing steps based on server stuff.

--- pinteresting/config/application.rb
+++ pinteresting/config/application.rb
@@ -10,6 +10,8 @@ module Pinteresting
   class Application < Rails::Application
 
     I18n.config.enforce_available_locales = true
+    #required for device / herokue 
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
